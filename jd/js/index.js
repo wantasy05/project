9ae63c5ed059a0995c5606248d9a593e33e93d63
wantasy <wantasy05@outlook.com>
2019-07-28 14:13:16 +0800

    首次提交

---
+++ jd/js/index.js
@@ -0,0 +1,170 @@
+
+
+$('li.my_jd').mouseover(function(){
+    $(this).children('.myjdlist').show();
+})
+$('li.my_jd').mouseout(function(){
+    $(this).children('.myjdlist').hide();
+})
+$('li.qy_cg').mouseover(function(){
+    $(this).children('.dropdown-layer').show();
+})
+$('li.qy_cg').mouseout(function(){
+    $(this).children('.dropdown-layer').hide();
+})
+$('li.kf_fw').mouseover(function(){
+    $(this).children('.dropdown-client').show();
+})
+$('li.kf_fw').mouseout(function(){
+    $(this).children('.dropdown-client').hide();
+})
+$('li.wz_dh').mouseover(function(){
+    $(this).children('.dropdown-web').show();
+})
+$('li.wz_dh').mouseout(function(){
+    $(this).children('.dropdown-web').hide();
+})
+$('li.sj_jd').mouseover(function(){
+    $(this).children('.mobile_pop_item').show();
+})
+$('li.sj_jd').mouseout(function(){
+    $(this).children('.mobile_pop_item').hide();
+})
+
+
+
+$('li.f1').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item1').show();
+})
+$('li.f1').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item1').hide();
+})
+$('li.f2').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item2').show();
+})
+$('li.f2').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item2').hide();
+})
+
+$('li.f3').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item3').show();
+})
+$('li.f3').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item3').hide();
+})
+$('li.f4').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item2').show();
+})
+$('li.f4').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item2').hide();
+})
+$('li.f5').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item1').show();
+})
+$('li.f5').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item1').hide();
+})
+$('li.f6').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item2').show();
+})
+$('li.f6').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item2').hide();
+})
+$('li.f7').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item1').show();
+})
+$('li.f7').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item1').hide();
+})
+$('li.f8').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item2').show();
+})
+$('li.f8').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item2').hide();
+})
+$('li.f9').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item1').show();
+})
+$('li.f9').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item1').hide();
+})
+$('li.f10').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item2').show();
+})
+$('li.f10').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item2').hide();
+})
+$('li.f11').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item1').show();
+})
+$('li.f11').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item1').hide();
+})
+$('li.f12').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item2').show();
+})
+$('li.f12').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item2').hide();
+})
+$('li.f13').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item1').show();
+})
+$('li.f13').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item1').hide();
+})
+$('li.f14').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item2').show();
+})
+$('li.f14').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item2').hide();
+})
+$('li.f15').mouseover(function(){
+    $('#J_popCtn').show();
+    $('#cate_item1').show();
+})
+$('li.f15').mouseout(function(){
+    $('#J_popCtn').hide();
+    $('#cate_item1').hide();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
